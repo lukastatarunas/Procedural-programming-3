8338aff1627fca92a3d18eba794edf7e9b6a75a1
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-06-14 12:56:50 +0300

    11.cpp

---
+++ 11.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int F(int n)
+{
+	if (n < 2)
+  {
+		return n;
+	}
+	else
+  {
+		return (F(n - 1) + F(n - 2));
+	}
+}
+
+int main()
+{
+	int n;
+	ifstream fin;
+	fin.open("11.in");
+	fin >> n;
+	fin.close();
+	ofstream fout;
+	fout.open("11.out");
+	fout << F(n);
+	fout.close();
+}
